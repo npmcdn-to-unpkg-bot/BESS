543e2cd4b5e74535124291fb6d5d6aed5ec8929f
Bram De Baerdemaeker <bram.debaerdemaeker@gmail.com>
2016-04-29 09:34:57 +0200

    front end

---
+++ Website/Site/js/views.js
@@ -0,0 +1,26 @@
+(function(){
+  var app = angular.module('view-templates', []);
+
+
+  app.directive("homepage", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "templates/homepage.html"
+    };
+  });
+
+  app.directive("card", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "templates/card.html"
+    };
+  });
+
+  app.directive("single_page", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "templates/single_page.html"
+    };
+  });
+
+})();
