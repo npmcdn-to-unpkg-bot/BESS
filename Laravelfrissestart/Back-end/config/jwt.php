30831a10eaebe1fb4daacb38c340c0007f41f789
edwwaarrdd <edward.vereertbrugghen@gmail.com>
2016-04-29 04:12:03 +0200

    Register plus login

--- Laravelfrissestart/Back-end/config/jwt.php
+++ Laravelfrissestart/Back-end/config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', 'Mcgt6YW84uUMmrAKZBPEBCitaHBZaETU'),
 
     /*
     |--------------------------------------------------------------------------
--- Laravelfrissestart/Back-end/database/migrations/2014_10_12_000000_create_users_table.php
+++ Laravelfrissestart/Back-end/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->nullable();
+            $table->string('firstname')->nullable();
+            $table->string('residence')->nullable();
+            $table->boolean('isAdmin');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
