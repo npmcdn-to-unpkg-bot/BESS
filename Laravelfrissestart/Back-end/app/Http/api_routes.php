1017dd8fae3af27131fb1130c2742c45a20b374f
edwwaarrdd <edward.vereertbrugghen@gmail.com>
2016-05-09 14:17:01 +0200

    API documentatie + wat routing problemen opgelost bij api

--- Laravelfrissestart/Back-end/app/Http/api_routes.php
+++ Laravelfrissestart/Back-end/app/Http/api_routes.php
@@ -6,18 +6,19 @@ $api->version('v1', function ($api) {
 
 	$api->post('auth/login', 'App\Api\V1\Controllers\AuthController@login');
 	$api->post('auth/signup', 'App\Api\V1\Controllers\AuthController@signup');
-	$api->post('auth/recovery', 'App\Api\V1\Controllers\AuthController@recovery');
-	$api->post('auth/reset', 'App\Api\V1\Controllers\AuthController@reset');
+	//$api->post('auth/recovery', 'App\Api\V1\Controllers\AuthController@recovery');
+	//$api->post('auth/reset', 'App\Api\V1\Controllers\AuthController@reset');
 
 	// example of protected route
 	$api->get('protected', ['middleware' => ['api.auth'], function () {
-		return \App\User::all();
+		//return \App\User::all();
+		return JWTAuth::parseToken()->authenticate();
 	}]);
 
 	// example of free route
-	$api->get('free', function() {
-		return \App\User::all();
-	});
+	// $api->get('free', function() {
+	// 	return \App\User::all();
+	// });
 
 	$api->group(['middleware' => 'api.auth'], function ($api) {
 		// projects routing api met auth
