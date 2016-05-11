e2d5e24fe89667bd31a88215e780546b56199e87
Bram De Baerdemaeker <bram.debaerdemaeker@gmail.com>
2016-05-11 14:21:44 +0200

    design
    layout button + tabselect
    font menu

--- Website/site2/js/app.js
+++ Website/site2/js/app.js
@@ -1,3 +1,18 @@
 (function() {
   var app = angular.module('inspraakStad', ['view-templates']);
+
+  app.controller("PanelController", function(){
+
+    this.tab = 1;
+
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+
+
 })();
