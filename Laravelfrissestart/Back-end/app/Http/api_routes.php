110d729041b4f7ab3b7800aa34c64d7652b1f0b7
edwwaarrdd <edward.vereertbrugghen@gmail.com>
2016-05-20 09:13:48 +0200

    user werkt terug oeps plus update gaat werken normaal
    Moet nog getest worden

--- Laravelfrissestart/Back-end/app/Http/api_routes.php
+++ Laravelfrissestart/Back-end/app/Http/api_routes.php
@@ -14,7 +14,7 @@ $api->post('auth/signup', 'App\Api\V1\Controllers\AuthController@signup');
 
 
 	// example of protected route
-	$api->get('protected', ['middleware' => ['api.auth', 'cors'], function () {
+	$api->get('user', ['middleware' => ['api.auth', 'cors'], function () {
 		//return \App\User::all();
 		return JWTAuth::parseToken()->authenticate();
 	}]);
@@ -29,17 +29,17 @@ $api->post('auth/signup', 'App\Api\V1\Controllers\AuthController@signup');
 		$api->get('projects/user', 'App\Api\V1\Controllers\ProjectController@indexuser');
 		$api->get('projects/user/{id}', 'App\Api\V1\Controllers\ProjectController@showuser');
 		$api->post('projects', 'App\Api\V1\Controllers\ProjectController@store');
-		$api->put('projects/{id}', 'App\Api\V1\Controllers\ProjectController@update');
+		$api->post('projects/{id}', 'App\Api\V1\Controllers\ProjectController@update');
 		$api->delete('projects/{id}', 'App\Api\V1\Controllers\ProjectController@destroy');
 
 		// question routing api met auth
 		$api->post('questions', 'App\Api\V1\Controllers\QuestionController@store');
-		$api->put('questions/{id}', 'App\Api\V1\Controllers\QuestionController@update');
+		$api->post('questions/{id}', 'App\Api\V1\Controllers\QuestionController@update');
 		$api->delete('questions/{id}', 'App\Api\V1\Controllers\QuestionController@destroy');
 
 		// timeline routing api met auth
 		$api->post('timelines', 'App\Api\V1\Controllers\TimelineController@store');
-		$api->put('timelines/{id}', 'App\Api\V1\Controllers\TimelineController@update');
+		$api->post('timelines/{id}', 'App\Api\V1\Controllers\TimelineController@update');
 		$api->delete('timelines/{id}', 'App\Api\V1\Controllers\TimelineController@destroy');
 
 		//answers routing api met auth
@@ -47,7 +47,7 @@ $api->post('auth/signup', 'App\Api\V1\Controllers\AuthController@signup');
 		$api->get('answers/user', 'App\Api\V1\Controllers\AnswerController@indexuser');
 		$api->get('answers/user/{id}', 'App\Api\V1\Controllers\AnswerController@showuser');
 		$api->post('answers', 'App\Api\V1\Controllers\AnswerController@store');
-		$api->put('answers/{id}', 'App\Api\V1\Controllers\AnswerController@update');
+		$api->post('answers/{id}', 'App\Api\V1\Controllers\AnswerController@update');
 		$api->delete('answers/{id}', 'App\Api\V1\Controllers\AnswerController@destroy');
 	});
 
