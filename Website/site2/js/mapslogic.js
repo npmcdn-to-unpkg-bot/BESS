5f67328ac10c59152eabec29b43a7c2e7f5c1d04
edwwaarrdd <edward.vereertbrugghen@gmail.com>
2016-05-23 17:32:31 +0200

    map wordt correct getoond en marker kan worden geplaatst

--- Website/site2/js/mapslogic.js
+++ Website/site2/js/mapslogic.js
@@ -8,14 +8,29 @@
 
     var mapslogic = this;
     mapslogic.getMapCreate = function() {
-      var mapcreate = new GMaps({
-        el: '#mapcreate',
-        lat: 51.2194475,
-        lng: 4.4024643,
-        zoom: 12,
-        width: '100%' ,
-        height: '400px'
-      });
+      if (mapcreate) {
+        mapcreate.refresh;
+        console.log("mapcreate bestaat al");
+      } else {
+        console.log("mapcreate word gemaakt");
+              var mapcreate = new GMaps({
+                el: '#mapcreate',
+                lat: 51.2194475,
+                lng: 4.4024643,
+                zoom: 12,
+                width: '100%' ,
+                height: '400px',
+                click: function(e) {
+                  mapcreate.removeMarkers();
+                  mapcreate.addMarker({
+                    lat: e.latLng.lat(),
+                    lng: e.latLng.lng(),
+                    title: 'Gekozen locatie'
+                  });
+                }
+              });
+      }
+
     };
 
     mapslogic.getMapOverview = function() {
