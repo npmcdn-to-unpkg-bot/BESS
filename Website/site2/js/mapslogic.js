45e9dbbffa6ea26ec894cb089ffb434ca250e9ae
edwwaarrdd <edward.vereertbrugghen@gmail.com>
2016-05-24 11:21:18 +0200

    maps icon

--- Website/site2/js/mapslogic.js
+++ Website/site2/js/mapslogic.js
@@ -3,6 +3,12 @@
   var app = angular.module('maps-logic-module', []);
 
   app.controller("mapsController", function($routeParams, $http, $scope, $location, $route){
+      var icon = {
+        url: '/img/A-logo.png', // url
+        scaledSize: new google.maps.Size(50, 50), // scaled size
+        origin: new google.maps.Point(0,0), // origin
+        anchor: new google.maps.Point(26, 26) // anchor
+      };
     var mapslogic = this;
     var mapcreate;
     mapslogic.getMapCreate = function() {
@@ -11,26 +17,27 @@
         console.log("mapcreate bestaat al");
       } else {
         console.log("mapcreate word gemaakt");
-              mapcreate = new GMaps({
-                el: '#mapcreate',
-                lat: 51.2194475,
-                lng: 4.4024643,
-                zoom: 12,
-                width: '100%' ,
-                height: '400px',
-                click: function(e) {
-                  mapslogic.lat = e.latLng.lat();
-                  mapslogic.lng = e.latLng.lng();
-                  console.log("lng = " + mapslogic.lat);
-                  console.log("lng = " + mapslogic.lng);
-                  mapcreate.removeMarkers();
-                  mapcreate.addMarker({
-                    lat: e.latLng.lat(),
-                    lng: e.latLng.lng(),
-                    title: 'Gekozen locatie'
-                  });
-                }
-              });
+        mapcreate = new GMaps({
+          el: '#mapcreate',
+          lat: 51.2194475,
+          lng: 4.4024643,
+          zoom: 12,
+          width: '100%' ,
+          height: '400px',
+          click: function(e) {
+            mapslogic.lat = e.latLng.lat();
+            mapslogic.lng = e.latLng.lng();
+            console.log("lng = " + mapslogic.lat);
+            console.log("lng = " + mapslogic.lng);
+            mapcreate.removeMarkers();
+            mapcreate.addMarker({
+              lat: e.latLng.lat(),
+              lng: e.latLng.lng(),
+              title: 'Gekozen locatie',
+              icon: icon
+            });
+          }
+        });
       }
 
     };
@@ -43,21 +50,31 @@
         zoom: 12
       });
     };
+    mapslogic.addMarkerToMapOverview = function(glatitude, glongtitude) {
+      map.addMarker({
+        lat: latitude,
+        lng: longitude,
+        title: 'Project locatie',
+        icon: icon
+      });
+    };
 
-        mapslogic.getMapSinglePage = function(latitude, longitude) {
-          var spmap = new GMaps({
-            el: '#spmap',
-            lat: latitude,
-            lng: longitude,
-            zoom: 15
-          });
-              spmap.removeMarkers();
-              spmap.addMarker({
-                lat: latitude,
-                lng: longitude,
-                title: 'Project locatie'
-              });
-        };
+    mapslogic.getMapSinglePage = function(latitude, longitude) {
+      var spmap = new GMaps({
+        el: '#spmap',
+        lat: latitude,
+        lng: longitude,
+        zoom: 15
+      });
+
+      spmap.removeMarkers();
+      spmap.addMarker({
+        lat: latitude,
+        lng: longitude,
+        title: 'Project locatie',
+        icon : icon,
+      });
+    };
 
   });
 
