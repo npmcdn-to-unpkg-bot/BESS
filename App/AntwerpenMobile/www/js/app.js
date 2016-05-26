44d87fb6a4b7c556abffee2a0131e112bae241c2
StevenSerrien <steven.serrien@student.kdg.be>
2016-05-26 13:48:04 +0200

    Navigation aanpassingen

--- App/AntwerpenMobile/www/js/app.js
+++ App/AntwerpenMobile/www/js/app.js
@@ -34,20 +34,22 @@ angular.module('ionicApp', ['ionic', 'ionic.contrib.ui.tinderCards', 'angular.fi
 
   //Conformiteiten rond IOS vs Android
   $ionicConfigProvider.tabs.position('bottom');
-  $ionicConfigProvider.navBar.alignTitle('center')
+  $ionicConfigProvider.navBar.alignTitle('center');
+  $ionicConfigProvider.backButton.previousTitleText(true).text('').icon('ion-android-arrow-back');
+
 
 
 })
 
 .directive('noScroll', function() {
-    return {
-        restrict: 'A',
-        link: function($scope, $element, $attr) {
-            $element.on('touchmove', function(e) {
-                e.preventDefault();
-            });
-        }
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attr) {
+      $element.on('touchmove', function(e) {
+        e.preventDefault();
+      });
     }
+  }
 })
 
 
@@ -58,7 +60,7 @@ angular.module('ionicApp', ['ionic', 'ionic.contrib.ui.tinderCards', 'angular.fi
     $state.go('intro');
   };
   $scope.formToggle = function() {
-       $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
   }
   //Disable back button after login $state.go
   $ionicHistory.nextViewOptions({
@@ -68,65 +70,65 @@ angular.module('ionicApp', ['ionic', 'ionic.contrib.ui.tinderCards', 'angular.fi
   var user = this;
   user.login = function(email, password){
     $http({
-        method : "POST",
-        url : "http://edwardvereertbrugghen.multimediatechnology.be/api/auth/login",
-        data: {
-          email: email,
-          password: password,
-        }
-      }).then(function mySucces(response) {
-        $state.go('intro');
-        localStorage.setItem("token", response.data.token);
-        user.getData();
-      }, function myError(response) {
-        console.log("login failed");
-      });
+      method : "POST",
+      url : "http://edwardvereertbrugghen.multimediatechnology.be/api/auth/login",
+      data: {
+        email: email,
+        password: password,
+      }
+    }).then(function mySucces(response) {
+      $state.go('intro');
+      localStorage.setItem("token", response.data.token);
+      user.getData();
+    }, function myError(response) {
+      console.log("login failed");
+    });
   }
 
   user.register = function(firstName, lastName, email, password){
 
-      $http({
-        method : "POST",
-        url : "http://edwardvereertbrugghen.multimediatechnology.be/api/auth/signup",
-        data: {
-          firstname: firstName,
-          name: lastName,
-          email: email,
-          password: password,
-        }
-      }).then(function mySucces(response) {
-        console.log("registratie succesvol token=" + response.data.token);
-        localStorage.setItem("token", response.data.token);
-        user.getData();
-        $state.go('intro');
-      }, function myError(response) {
-        console.log("register failed");
-      });
-    };
+    $http({
+      method : "POST",
+      url : "http://edwardvereertbrugghen.multimediatechnology.be/api/auth/signup",
+      data: {
+        firstname: firstName,
+        name: lastName,
+        email: email,
+        password: password,
+      }
+    }).then(function mySucces(response) {
+      console.log("registratie succesvol token=" + response.data.token);
+      localStorage.setItem("token", response.data.token);
+      user.getData();
+      $state.go('intro');
+    }, function myError(response) {
+      console.log("register failed");
+    });
+  };
 
 
 
   user.getData = function(){
-      if (localStorage.token) {
-        $http({
-          method: "GET",
-          url: "http://edwardvereertbrugghen.multimediatechnology.be/api/user?token=" + localStorage.token
-        }).then(function mySucces(response) {
-          user.loggedin = true;
-          localStorage.setItem("firstname", response.data.user.firstname);
-          localStorage.setItem("lastname", response.data.user.name);
-          localStorage.setItem("email", response.data.user.email);
-          localStorage.setItem("isAdmin", response.data.user.isAdmin);
-          localStorage.setItem("residence", response.data.user.residence);
-          user.firstname = localStorage.firstname;
-          user.isAdmin = localStorage.isAdmin;
-          console.log(user.firstname);
-        }, function myError(response) {
-          console.log("User ophalen failed");
-          user.logout();
-        });
-      }
-    };
+    if (localStorage.token) {
+      $http({
+        method: "GET",
+        url: "http://edwardvereertbrugghen.multimediatechnology.be/api/user?token=" + localStorage.token
+      }).then(function mySucces(response) {
+        user.loggedin = true;
+        localStorage.setItem("firstname", response.data.user.firstname);
+        localStorage.setItem("lastname", response.data.user.name);
+        localStorage.setItem("email", response.data.user.email);
+        localStorage.setItem("isAdmin", response.data.user.isAdmin);
+        localStorage.setItem("residence", response.data.user.residence);
+        user.firstname = localStorage.firstname;
+        user.isAdmin = localStorage.isAdmin;
+        console.log(user.firstname);
+      }, function myError(response) {
+        console.log("User ophalen failed");
+        user.logout();
+      });
+    }
+  };
 
 
 })
@@ -161,103 +163,111 @@ angular.module('ionicApp', ['ionic', 'ionic.contrib.ui.tinderCards', 'angular.fi
   }
 
 
+
+
   //Loading
   $ionicLoading.show({
     template: 'Loading...'
   });
   //API Call voor alle projecten
   HttpService.getProjects()
-    .then(function(response) {
-       $scope.projects = response;
-       $ionicLoading.hide();
-    });
+  .then(function(response) {
+    $scope.projects = response;
+    $ionicLoading.hide();
+  });
 })
 
-.controller('ProjectDetailCtrl', function($scope, $state, $stateParams, HttpService, $ionicModal) {
+.controller('ProjectDetailCtrl', function($scope, $state, $stateParams, HttpService, $ionicModal, $ionicHistory) {
   var projectId = $stateParams.projectId;
   $scope.projectId = projectId;
 
+  $scope.toMain = function() {
+    $ionicHistory.goBack();
+  }
   HttpService.getProjectDetail(projectId)
-    .then(function(response) {
-       $scope.projectDetail = response.project;
-      console.log($scope.projectDetail);
-    });
+  .then(function(response) {
+    $scope.projectDetail = response.project;
+    console.log($scope.projectDetail);
+  });
 
 
 
 })
 
 
-.controller('ProjectDetailTinderCtrl', function($scope, $state, $stateParams, HttpService, $ionicModal) {
+.controller('ProjectDetailTinderCtrl', function($scope, $state, $stateParams, HttpService, $ionicModal, $ionicHistory) {
   var projectId = $stateParams.projectId;
 
+  $scope.toDetail = function() {
+    $ionicHistory.goBack();
+  }
 
 
 
   HttpService.getProjectDetail(projectId)
-    .then(function(response) {
-       $scope.projectDetail = response.project;
-      console.log($scope.projectDetail);
-    });
+  .then(function(response) {
+    $scope.projectDetail = response.project;
+    console.log($scope.projectDetail);
+  });
 
-    HttpService.getQuestionsByProject(projectId)
-      .then(function(response) {
-         $scope.questions = response.questions;
-        var cardTypes = $scope.questions;
-        $scope.remainingQuestions = false;
+  HttpService.getQuestionsByProject(projectId)
+  .then(function(response) {
+    $scope.questions = response.questions;
+    var cardTypes = $scope.questions;
+    $scope.remainingQuestions = false;
 
-        $scope.cards = [];
+    $scope.cards = [];
 
 
-        $scope.addCard = function(i) {
+    $scope.addCard = function(i) {
 
-          if (cardTypes[i].kind ==='yesno') {
-            var newCard = cardTypes[i];
-            newCard.id = i;
-            $scope.cards.push(angular.extend({}, newCard));
-          } else {
+      if (cardTypes[i].kind ==='yesno') {
+        var newCard = cardTypes[i];
+        newCard.id = i;
+        $scope.cards.push(angular.extend({}, newCard));
+      } else {
 
-          }
+      }
 
-        }
+    }
 
-        for(var i = 0; i < cardTypes.length; i++) $scope.addCard(i);
+    for(var i = 0; i < cardTypes.length; i++) $scope.addCard(i);
 
-        $scope.cardSwipedLeft = function(index, projectid, questionid) {
-            HttpService.answerNo(index, projectid, questionid);
-            console.log('Left swipe');
-            console.log('project id = ' +projectid +" question id= "+questionid);
-        }
+    $scope.cardSwipedLeft = function(index, projectid, questionid) {
+      HttpService.answerNo(index, projectid, questionid);
+      console.log('Left swipe');
+      console.log('project id = ' +projectid +" question id= "+questionid);
+    }
 
-        $scope.cardSwipedRight = function(index, projectid, questionid) {
-            HttpService.answerYes(index, projectid, questionid);
-            console.log('Right swipe');
-            console.log('project id = ' +projectid +" question id= "+questionid);
-        }
+    $scope.cardSwipedRight = function(index, projectid, questionid) {
+      HttpService.answerYes(index, projectid, questionid);
+      console.log('Right swipe');
+      console.log('project id = ' +projectid +" question id= "+questionid);
+    }
 
-        $scope.cardDestroyed = function(index) {
-            $scope.cards.splice(index, 1);
-            if ($scope.cards.length == 0) {
-              $scope.remainingQuestions = true;
-            }
-            console.log('Card removed');
-        }
-      }, function myError(response) {
+    $scope.cardDestroyed = function(index) {
+      $scope.cards.splice(index, 1);
+      if ($scope.cards.length == 0) {
         $scope.remainingQuestions = true;
-      });
+      }
+      console.log('Card removed');
+    }
+  }, function myError(response) {
+    $scope.remainingQuestions = true;
+  });
 
 
-    $ionicModal.fromTemplateUrl('templates/modal-tinder.html', {
-      scope: $scope
-        }).then(function(modal) {
-          $scope.modal = modal;
-        });
+  $ionicModal.fromTemplateUrl('templates/modal-tinder.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
 
-    $scope.showSwipeTutorial = function() {
-          $scope.modal.show();
-    };
+  $scope.showSwipeTutorial = function() {
+    $scope.modal.show();
+  };
 
-    /* CARD-SECTION*/
+  /* CARD-SECTION*/
 
 
 
@@ -269,88 +279,88 @@ angular.module('ionicApp', ['ionic', 'ionic.contrib.ui.tinderCards', 'angular.fi
   var projectId = $stateParams.projectId;
   var images = this;
 
-      images.getfirstbyid = function(gid) {
-      images.firstonebyid = [];
-      $http.get("http://edwardvereertbrugghen.multimediatechnology.be/api/image/project/"+ gid)
-      .then(function mySucces(response) {
+  images.getfirstbyid = function(gid) {
+    images.firstonebyid = [];
+    $http.get("http://edwardvereertbrugghen.multimediatechnology.be/api/image/project/"+ gid)
+    .then(function mySucces(response) {
 
-        if (response.data.images) {
+      if (response.data.images) {
 
-          images.firstonebyid[gid] = "http://edwardvereertbrugghen.multimediatechnology.be/uploads/" + response.data.images[response.data.images.length - 1].filename;
-          console.log("gid adres image = "+ images.getfirstbyid[gid]);
-        }
-        else {
-          console.log("image fetch failed probably because image does not exist yet.");
-          images.firstonebyid[gid] = null;
-        }
-
-      }, function myError(response) {
-        console.log("sever error.");
+        images.firstonebyid[gid] = "http://edwardvereertbrugghen.multimediatechnology.be/uploads/" + response.data.images[response.data.images.length - 1].filename;
+        console.log("gid adres image = "+ images.getfirstbyid[gid]);
+      }
+      else {
+        console.log("image fetch failed probably because image does not exist yet.");
         images.firstonebyid[gid] = null;
-      });
-    };
+      }
+
+    }, function myError(response) {
+      console.log("sever error.");
+      images.firstonebyid[gid] = null;
+    });
+  };
 })
 
 
 
 .service('HttpService', function($http) {
- return {
-   getProjects: function() {
-     return $http.get("http://edwardvereertbrugghen.multimediatechnology.be/api/projects")
-         .then(function (response) {
-         console.log('Get projects', response);
-         return response.data;
-       });
-   },
-   getProjectDetail: function(id) {
-     // $http returns a promise, which has a then function, which also returns a promise.
-     return $http.get('http://edwardvereertbrugghen.multimediatechnology.be/api/projects/' + id)
-       .then(function(response) {
-         // In the response, resp.data contains the result. Check the console to see all of the data returned.
-         console.log('Get Project detail', response);
-         return response.data;
+  return {
+    getProjects: function() {
+      return $http.get("http://edwardvereertbrugghen.multimediatechnology.be/api/projects")
+      .then(function (response) {
+        console.log('Get projects', response);
+        return response.data;
       });
-   },
-   getQuestionsByProject: function(id) {
-     // $http returns a promise, which has a then function, which also returns a promise.
-     return $http.get(' http://edwardvereertbrugghen.multimediatechnology.be/api/questions/project/' + id)
-       .then(function(response) {
-         // In the response, resp.data contains the result. Check the console to see all of the data returned.
-         console.log('Get questions by project', response);
-         return response.data;
+    },
+    getProjectDetail: function(id) {
+      // $http returns a promise, which has a then function, which also returns a promise.
+      return $http.get('http://edwardvereertbrugghen.multimediatechnology.be/api/projects/' + id)
+      .then(function(response) {
+        // In the response, resp.data contains the result. Check the console to see all of the data returned.
+        console.log('Get Project detail', response);
+        return response.data;
       });
-   },
-   answerYes: function(index, projectId, questionId) {
+    },
+    getQuestionsByProject: function(id) {
+      // $http returns a promise, which has a then function, which also returns a promise.
+      return $http.get(' http://edwardvereertbrugghen.multimediatechnology.be/api/questions/project/' + id)
+      .then(function(response) {
+        // In the response, resp.data contains the result. Check the console to see all of the data returned.
+        console.log('Get questions by project', response);
+        return response.data;
+      });
+    },
+    answerYes: function(index, projectId, questionId) {
 
-       $http({
-         method : "POST",
-         url : "http://edwardvereertbrugghen.multimediatechnology.be/api/answers?token=" + localStorage.token,
-         data: {
-           answer: "yes",
-           question_id: questionId,
-           project_id: projectId,
-         }
-       }).then(function mySucces(response) {
-          console.log("Antwoord API JA gelukt.");
-       }, function myError(response) {
-         console.log("ERROR bij JA-antwoord");
-       });
-     },
-     answerNo: function(index, projectId, questionId) {
+      $http({
+        method : "POST",
+        url : "http://edwardvereertbrugghen.multimediatechnology.be/api/answers?token=" + localStorage.token,
+        data: {
+          answer: "yes",
+          question_id: questionId,
+          project_id: projectId,
+        }
+      }).then(function mySucces(response) {
+        console.log("Antwoord API JA gelukt.");
+      }, function myError(response) {
+        console.log("ERROR bij JA-antwoord");
+      });
+    },
+    answerNo: function(index, projectId, questionId) {
 
-         $http({
-           method : "POST",
-           url : "http://edwardvereertbrugghen.multimediatechnology.be/api/answers?token=" + localStorage.token,
-           data: {
-             answer: "no",
-             question_id: questionId,
-             project_id: projectId,
-           }
-         }).then(function mySucces(response) {
-            console.log("Antwoord API NEE gelukt");
-         }, function myError(response) {
-           console.log("ERROR bij NEE-antwoord.");
-         });
-       }
- };
+      $http({
+        method : "POST",
+        url : "http://edwardvereertbrugghen.multimediatechnology.be/api/answers?token=" + localStorage.token,
+        data: {
+          answer: "no",
+          question_id: questionId,
+          project_id: projectId,
+        }
+      }).then(function mySucces(response) {
+        console.log("Antwoord API NEE gelukt");
+      }, function myError(response) {
+        console.log("ERROR bij NEE-antwoord.");
+      });
+    }
+  };
 });
